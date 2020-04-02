1421aee98c5b8f3b4e6c795740c69b086d95abf9
dortlii <fabian@dort.sh>
2020-04-02 13:58:06 +0200

    Added function for AD structure

--- AD_Prep_with_Shares.ps1
+++ AD_Prep_with_Shares.ps1
@@ -3,8 +3,8 @@
 Skriptname:     AD_Prep.ps1
 Funktion:       Anlegen der Basisstruktur in der AD
 Erstellt am:    25.11.2014
-Author:         N.Viragh
-Version:        1.0
+Author:         N.Viragh / F. Dort
+Version:        1.1
 
 Aenderungen:
 23.3.2020       Fehler korrigiert
@@ -13,55 +13,29 @@ Aenderungen:
 ##########################################################################
 
 # Variabeln deklarieren
+$module = "adprep.psm1"
 
-
+# Modul importieren
+Import-Module $module -Force
 
 # Abfrage Domänenname
 
-$domainname = Read-Host "Geben Sie den Domänennamen ein.(ohne TLD)"
+$domainname = Read-Host "Geben Sie den Domänennamen ein.(ohne TLD) > "
 
 # Abfrage TDL
 
-$TLD = Read-Host "Geben Sie die TLD ein."
+$TLD = Read-Host "Geben Sie die TLD ein. > "
 
 # Abfrage Name erste OU
 
-$firstOU = Read-Host "Geben Sie den Namen der Ersten OU ein."
-
-# Erstellung der OU's
-
-New-ADOrganizationalUnit -Name $firstOU -Path "DC=$domainname,DC=$TLD"
-
-# Struktur Global 
-
-New-ADOrganizationalUnit -Name "Global" -Path "OU=$firstOU,DC=$domainname,DC=$TLD"
-
-New-ADOrganizationalUnit -Name "Laufwerke" -Path "OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Mitgliedserver" -Path "OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Gruppen" -Path "OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Benutzer" -Path "OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Gateway" -Path "OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Kontakte" -Path "OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-
-# Unterstruktur Global\Gruppen erstellen
-
-New-ADOrganizationalUnit -Name "Systemgruppen" -Path "OU=Gruppen,OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Dateiberechtigungsgruppen" -Path "OU=Gruppen,OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Druckerzuweisung" -Path "OU=Gruppen,OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Softwaregruppen" -Path "OU=Gruppen,OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Outlookberechtigungsgruppen" -Path "OU=Gruppen,OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Verteilerliste" -Path "OU=Gruppen,OU=Global,OU=$firstOU,DC=$domainname,DC=$TLD"
+$firstOU = Read-Host "Geben Sie den Namen der ersten OU ein. > "
 
 # Abfrage Standortname
 
-$standort = Read-Host "Geben Sie den Standortnamen ein."
+$standorte = Read-Host "Geben Sie die Standorte ein. > "
 
-# Struktur Standort anlegen
-
-New-ADOrganizationalUnit -Name $standort -Path "OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Benutzer" -Path "OU=$standort,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Notebook" -Path "OU=$standort,OU=$firstOU,DC=$domainname,DC=$TLD"
-New-ADOrganizationalUnit -Name "Computer" -Path "OU=$standort,OU=$firstOU,DC=$domainname,DC=$TLD"
+# Funktion aufrufen und Parameter mitgeben
+New-ADStructure -domain $domainname -tld $TLD -firstOU $firstOU -standorte $standorte
 
 ##################################################################################################
 
